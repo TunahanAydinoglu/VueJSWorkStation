5eacb2ad376787d2b845d7c54c3a8522e00e5690
Tunahan <tunahanaydinoglu@outlook.com>
2021-01-09 03:04:26 +0300

    event Emit

---
+++ basic-start/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <ProductList @delete:product="deleteProduct" :products="products"/>
+  </div>
+</template>
+
+<script>
+import ProductList from "./components/ProductList.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          categoryId: 1,
+          productName: "Classical Guitar",
+          description: "Cordoba Guitar",
+          unitPrice: 375,
+          unitsInStock: 2,
+        },
+        {
+          id: 2,
+          categoryId: 3,
+          productName: "Drum Set",
+          description: "Yamaha",
+          unitPrice: 375,
+          unitsInStock: 2,
+        },
+        {
+          id: 3,
+          categoryId:1,
+          productName: "Acoustic Guitar",
+          description: "Yamaha Guitar",
+          unitPrice: 450,
+          unitsInStock: 3,
+        },
+        {
+          id: 4,
+          categoryId: 2,
+          productName: "Piano",
+          description: "Pearl River Piano",
+          unitPrice: 8765,
+          unitsInStock: 1,
+        },
+      ],
+    };
+  },
+  methods:{
+    deleteProduct(product){
+      this.products = this.products.filter(
+        productToFilter => productToFilter.id !== product.id
+      )
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ basic-start/src/components/ProductList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <p v-if="products.length==0">The List is Empty</p>
+    <table class="table" v-else>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Product Name</th>
+          <th>Category Id</th>
+          <th>Description</th>
+          <th>Unit Price</th>
+          <th>Stock</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+            <td>{{product.id}}</td>
+            <td>{{product.productName}}</td>
+            <td>{{product.categoryId}}</td>
+            <td>{{product.description}}</td>
+            <td>{{product.unitPrice}}</td>
+            <td>{{product.unitsInStock}}</td>
+            <td><button class="btn btn-sm btn-danger" @click="handleDelete(product)">Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name:"product-list",
+    props:{
+      products:Array
+    },
+    methods:{
+      handleDelete(product){
+        this.$emit("delete:product",product)
+      },
+      handleUpdate(){
+
+      }
+    }
+});
+</script>
+
+<style scoped></style>
